082fc4b559da4cb8e2ae5018fb7115ac15a0e035
Karl Pablo <me@karlpablo.com>
2023-09-10 07:54:01 -0700

    Better HomeBanner

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -2,13 +2,13 @@ export default function HeroBanner({ onGetStarted }) {
   return (
     <div className="hero h-full bg-neutral text-neutral-content rounded">
         <div className="space-y-8 text-center">
-          <h1 className="text-4xl lg:text-5xl w-48 lg:w-auto text-center font-serif leading-snug">
-            Find the best auto lease rates.
+          <h1 className="text-4xl lg:text-5xl w-auto px-6 text-center font-serif leading-snug">
+            Find the best auto lease rates!
           </h1>
           <button className="lg:hidden btn btn-primary" onClick={onGetStarted}>Get Started</button>
-          <div className="hidden lg:block text-lg">
-            <button className="btn btn-ghost" onClick={onGetStarted}>&larr; Get started</button>
-            </div>
+          <div className="hidden lg:block">
+            <button className="btn btn-lg btn-primary" onClick={onGetStarted}>Get Started</button>
+          </div>
         </div>
     </div>
   )
